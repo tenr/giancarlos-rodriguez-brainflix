0b57e145069fd842870a53f99405beb8a99ed0e0
tenr <grodriguez1224@gmail.com>
2023-04-04 16:15:43 -0400

    upadting style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import "./App.css";
+import "./App.scss";
+import Header from "./components/Header/Header";
+import "./styles/partials/_globals.scss";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <div>Hello World</div>
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div>
+      <nav></nav>
+    </div>
+  );
+}
+
+export default Header;
